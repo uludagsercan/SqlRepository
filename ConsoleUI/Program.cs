9e61d29880988899a6402f0c62bcd6156d69ae6f
Sercan <sercan.uludag91@hotmail.com>
2021-11-02 22:51:11 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,62 @@
+﻿using SqlFramework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> str = new List<Student>();
+            str.Add(new Student() { StudentId = 3 });
+            Student st = new Student();
+            //st.StudentName = "Serdcamsad";
+            st.StudentName = "Sercan";
+            st.StudentId = 4;
+            str.Add(new Student { StudentId = 1, StudentName = "Sercan" });
+            SqlRepository<Student> sqlRepository = new SqlRepository<Student>       
+               (@"Server=DESKTOP-H4NELET\SQLEXPRESS;Database=Deneme;Trusted_Connection=True;");
+            Student st1 = new Student();
+            st1.StudentId = 12;
+            st1.StudentName = "Mehmet";
+            Student st2 = new Student();
+            st2.StudentId = 18;
+            st2.StudentName = "Halit";
+            Student st3 = new Student();
+            st3.StudentId = 15;
+            st3.StudentName = "İbrahim";
+            SqlRepository<Person> sqlRepository1 = new SqlRepository<Person>
+               (@"Server=DESKTOP-H4NELET\SQLEXPRESS;Database=Deneme;Trusted_Connection=True;");
+            //Person p1 = new Person();
+            //p1.PersonName = "Sercan";
+            //p1.PersonSurname = "Uludag";
+            //sqlRepository1.Insert(p1);
+            Person p2 = new Person();
+            p2.PersonId = 1;
+            p2.PersonName = "Alper";
+            p2.PersonSurname = "Aktaş";
+            sqlRepository1.Update(p2);
+            Student st4 = new Student();
+            st4.StudentName = "muhammet";
+            sqlRepository.Insert(st4);
+            // sqlRepository.Insert(st);
+           
+            var results =sqlRepository.GetAll();
+            var result2 = results.FirstOrDefault(x => x.StudentId == 15);
+            Console.WriteLine(result2.StudentName);
+            sqlRepository.Update(st);
+            sqlRepository.Update(st1);
+            sqlRepository.Update(st2);
+            sqlRepository.Update(st3);
+
+            sqlRepository.Delete(st1);
+        }
+      
+    }
+}
---
+++ SqlFramework/SqlRepository.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlFramework
+{
+    public class SqlRepository<T> where T : class, new()
+    {
+
+        SqlConnection _connection;
+        private T GetT { get; set; }
+        private string TableName { get; set; }
+        private string Key { get; set; }
+        private string Value { get; set; }
+        Dictionary<object, object> datas { get; set; }
+
+        //private Dictionary<object, object> datas { get; set; }
+        private List<string> KeyAttributes { get; set; }
+        public SqlRepository(string _connection)
+        {
+            GetTableNameByClass(new T());
+            Key = "";
+            Value = "";
+            GetT = new T();
+            this._connection = new SqlConnection(_connection);
+        }
+        public SqlRepository()
+        {
+            
+        }
+        private void GetTableNameByClass(T tableClass)
+        {
+            datas = new Dictionary<object, object>();
+            var attrs = tableClass.GetType().GetCustomAttributes(false);
+            foreach (Attribute attr in attrs)
+            {
+                TableAttribute tAttr = attr as TableAttribute;
+                if (tAttr != null)
+                {
+                    if (tAttr.Schema == "dbo")
+                    {
+                        TableName = tAttr.Name;
+                    }
+                }
+            }
+        }
+        public void Insert(T data)
+        {
+            CreateAddQuery(data);
+            try
+            {
+                _connection.Open();
+                string cmd = String.Format(@"Insert Into {0} ({1}) Values ({2})", TableName, Key, Value);
+                SqlCommand command = new SqlCommand(cmd, _connection);
+                foreach (var item in datas)
+                {
+                    command.Parameters.AddWithValue("@" + (string)item.Key, item.Value);
+                }
+                command.ExecuteNonQuery();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("[Error Occurred]  Error Message : {0}", e.Message);
+            }
+           
+            finally
+            {
+                _connection.Close();
+            }
+        }
+        private void CreateAddQuery(T data)
+        {
+            this.Key = "";
+            this.Value = "";
+            datas = new Dictionary<object, object>();
+            KeyAttributes = new List<string>();
+            foreach (var prop in data.GetType().GetProperties())
+            {
+                foreach (object keyAttr in prop.GetCustomAttributes(true))
+                {
+
+                    KeyAttribute kAttr = keyAttr as KeyAttribute;
+                    if (kAttr != null)
+                    {
+                        KeyAttributes.Add(prop.Name);
+                    }
+                }
+            }
+
+            foreach (var item in data.GetType().GetProperties())
+            {
+
+                datas.Add(item.Name, item.GetValue(data));
+            }
+
+            foreach (var item in datas)
+            {
+                if (KeyAttributes.Contains(item.Key))
+                {
+                    Key += "";
+                }
+                else
+                {
+                    Key += item.Key;
+                    Key += ",";
+                    Value += "@";
+                    Value += item.Key;
+                    Value += ",";
+                }
+            }
+            if (Key.Length > 0)
+            {
+                Key = Key.Substring(0, (Key.Length - 1));
+                Value = Value.Substring(0, (Value.Length - 1));
+            }
+        }
+
+        public List<T> GetAll()
+        {
+            CreateSelectQuery();
+            string cmd = String.Format("Select {0} from {1}", Key, TableName);
+            SqlCommand command = new SqlCommand(cmd, _connection);
+            List<T> entities = new List<T>();
+            try
+            {      
+                _connection.Open();
+                SqlDataReader dr = command.ExecuteReader();
+                PropertyInfo[] properties = GetT.GetType().GetProperties();
+                Hashtable hashtable = new Hashtable();
+               
+                foreach (PropertyInfo propertyInfo in properties)
+                {
+                    hashtable.Add(propertyInfo.Name, propertyInfo);
+                }
+                while (dr.Read())
+                {
+                    T entity = new T();
+                    for (int i = 0; i < dr.FieldCount; i++)
+                    {
+                        PropertyInfo info = (PropertyInfo)hashtable[dr.GetName(i)];
+                        if (info != null)
+                        {
+                            info.SetValue(entity, dr.GetValue(i), null);
+                        }
+                    }
+                    entities.Add(entity);
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("[Error Occurred]  Error Message : {0}", e.Message);
+
+            }
+            finally
+            {
+                _connection.Close();
+               
+            }
+            return entities;
+
+        }
+
+        private void CreateSelectQuery()
+        {
+            this.Key = "";
+            this.Value = "";
+            datas = new Dictionary<object, object>();
+            KeyAttributes = new List<string>();
+            foreach (var prop in GetT.GetType().GetProperties())
+            {
+                foreach (object keyAttr in prop.GetCustomAttributes(true))
+                {
+
+                    KeyAttribute kAttr = keyAttr as KeyAttribute;
+                    if (kAttr != null)
+                    {
+                        KeyAttributes.Add(prop.Name);
+                    }
+                }
+            }
+            foreach (var item in GetT.GetType().GetProperties())
+            {
+
+                datas.Add(item.Name, item.GetValue(GetT));
+            }
+
+            foreach (var item in datas)
+            {
+                Key += item.Key;
+                Key += ",";
+                Value += "@";
+                Value += item.Key;
+                Value += ",";
+            }
+            if (Key.Length > 0)
+            {
+                Key = Key.Substring(0, (Key.Length - 1));
+                Value = Value.Substring(0, (Value.Length - 1));
+            }
+        }
+
+        public void Update(T data)
+        {
+            CreateUpdateQuery(data);
+            try
+            {
+                _connection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[Error Occurred] Error Message : {0}", e.Message);
+
+            }
+            string cmd = String.Format("Update {0} Set {1} where {2} = @{3}", TableName, Key, Value, Value);
+            SqlCommand command = new SqlCommand(cmd, _connection);
+            foreach (var item in datas)
+            {
+                command.Parameters.AddWithValue("@" + (string)item.Key, item.Value);
+            }
+            command.ExecuteNonQuery();
+            _connection.Close();
+        }
+
+        private void CreateUpdateQuery(T data)
+        {
+            Key = "";
+            Value = "";
+            datas = new Dictionary<object, object>();
+            KeyAttributes = new List<string>();
+            foreach (var prop in data.GetType().GetProperties())
+            {
+                foreach (object keyAttr in prop.GetCustomAttributes(true))
+                {
+
+                    KeyAttribute kAttr = keyAttr as KeyAttribute;
+                    if (kAttr != null)
+                    {
+                        KeyAttributes.Add(prop.Name);
+                    }
+                }
+            }
+
+            foreach (var item in data.GetType().GetProperties())
+            {
+
+                datas.Add(item.Name, item.GetValue(data));
+            }
+
+            foreach (var item in datas)
+            {
+                if (KeyAttributes.Contains(item.Key))
+                {
+                    Key += "";
+                    Value += item.Key;
+                }
+                else
+                {
+                    Key += item.Key;
+                    Key += "=";
+                    Key += "@";
+
+                    Key += item.Key;
+                    Key += ",";
+                }
+            }
+            if (Key.Length > 0)
+            {
+                Key = Key.Substring(0, (Key.Length - 1));
+
+            }
+
+            
+        }
+
+        public void Delete(T data)
+        {
+            CreateDeleteQuery(data);
+            try
+            {
+                _connection.Open();
+                string cmd = String.Format("Delete From {0} Where {1}=@{2}", TableName, Key, Value);
+                SqlCommand command = new SqlCommand(cmd, _connection);
+                foreach (var item in datas)
+                {
+                    command.Parameters.AddWithValue("@" + (string)item.Key, item.Value);
+                }
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("[Error Occurred]  Error Message : {0}", e.Message);
+
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+        }
+
+        private void CreateDeleteQuery(T data)
+        {
+            Key = "";
+            Value = "";
+            datas = new Dictionary<object, object>();
+            KeyAttributes = new List<string>();
+            foreach (var prop in data.GetType().GetProperties())
+            {
+                foreach (object keyAttr in prop.GetCustomAttributes(true))
+                {
+
+                    KeyAttribute kAttr = keyAttr as KeyAttribute;
+                    if (kAttr != null)
+                    {
+                        KeyAttributes.Add(prop.Name);
+                    }
+                }
+            }
+            foreach (var item in data.GetType().GetProperties())
+            {
+
+                datas.Add(item.Name, item.GetValue(data));
+            }
+            foreach (var item in datas)
+            {
+                if (KeyAttributes.Contains(item.Key))
+                {
+                    Key += item.Key;
+                    Value += item.Key;
+                }
+
+            }
+
+           
+        }
+    }
+}
